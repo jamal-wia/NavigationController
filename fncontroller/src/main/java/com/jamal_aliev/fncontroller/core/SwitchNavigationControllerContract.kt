08e6f7b2c1b86ad47ef9dd0f2b65c49ffb0d1bd4
Jamal <aliev.djamal.2000@gmail.com>
2023-11-22 16:10:14 +0300

    fix navigations contracts

--- fncontroller/src/main/java/com/jamal_aliev/fncontroller/core/SwitchNavigationControllerContract.kt
+++ fncontroller/src/main/java/com/jamal_aliev/fncontroller/core/SwitchNavigationControllerContract.kt
@@ -1,12 +1,16 @@
 package com.jamal_aliev.fncontroller.core
 
+import com.jamal_aliev.fncontroller.core.provider.ContainerProvider
 import com.jamal_aliev.fncontroller.core.provider.NavigationScreenSwitcherProvider
+import com.jamal_aliev.fncontroller.core.provider.OnNavigationUpProvider
 import com.jamal_aliev.fncontroller.core.screen.SwitchScreen
 
 /**
  * @author Jamal Aliev (aliev.djamal.2000@gmail.com)
  */
-interface SwitchNavigationControllerContract : NavigationControllerContract,
+interface SwitchNavigationControllerContract :
+    ContainerProvider,
+    OnNavigationUpProvider,
     NavigationScreenSwitcherProvider {
     fun onSwitchScreen(
         screenFrom: SwitchScreen?,
