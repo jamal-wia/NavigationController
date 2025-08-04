ec37f7670173163914c78a4785dcb5a3d0d15730
Jamal <aliev.djamal.2000@gmail.com>
2025-08-04 10:43:41 +0300

    "Version" - updated 1.1.4 -> 1.1.5: - fixed LineNavigationControllerBottomDialog - fixed LineNavigationControllerDialog - fixed NavigationControllerFragment

--- navigationcontroller/build.gradle.kts
+++ navigationcontroller/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.jamal-wia"
-version = "1.1.4"
+version = "1.1.5"
 
 android {
     namespace = "com.jamal_aliev.navigationcontroller"
@@ -53,7 +53,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.jamal-wia"
                 artifactId = "navigationcontroller"
-                version = "1.1.4"
+                version = "1.1.5"
             }
         }
     }
